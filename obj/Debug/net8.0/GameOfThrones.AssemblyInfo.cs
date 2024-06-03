33f66476ff37987b5d10db483cfbd26a90534b99
Natan Boanafina <boanafina.natan@gmail.com>
2024-06-03 17:49:32 -0300

    gitignore added

--- obj/Debug/net8.0/GameOfThrones.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameOfThrones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameOfThrones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55999e9a1cdffeb320e760a649d7c84e548dc91a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602a28c86aa5f446b2998f190953f0d19294c55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameOfThrones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameOfThrones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
