efc406cdfd0d7df96bf6eef06c42bc66c20b90c1
Natan Boanafina <boanafina.natan@gmail.com>
2024-06-06 11:01:40 -0300

    JWT configured

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using DotNetEnv;
 
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +12,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<GameOfThronesContext>(options =>
 {
     DbContextOptionsBuilder dbContextOptionsBuilder = options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddTransient<IFileService, FileService>();
 var app = builder.Build();
+
+// Configuring JWT
+// ?? throw new ... funciona da mesma forma que uma validação com if
+var key = Environment.GetEnvironmentVariable("JWT_KEY") ?? throw new InvalidOperationException("JWT_KEY environment variable is not set.");
+var keyBytes = Encoding.ASCII.GetBytes(key);
+
+// Adding services to the container
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "issuer",
+        ValidAudience = "audience",
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes)
+    };
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
